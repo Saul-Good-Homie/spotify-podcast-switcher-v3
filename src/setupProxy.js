dac036ec090084b721fbc2a47b9b4abc326ea7b8
Saul Belisle <rbelisle1270@gmail.com>
2023-03-21 17:41:31 -0500

    proxy middleware if statement

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,30 @@
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// module.exports = function (app) {
+//     app.use('/auth/**', 
+//         createProxyMiddleware({ 
+//             target: 'http://localhost:8888'
+//         })
+//     );
+// };
+
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    app.use('/auth/**', 
-        createProxyMiddleware({ 
-            target: 'http://localhost:8888'
-        })
-    );
+  // Detect whether app is running in development mode
+  const isDevelopment = process.env.NODE_ENV === 'development';
+
+  // Set the proxy target URL based on whether app is in development mode or not
+  const target = isDevelopment
+    ? 'http://localhost:8888'
+    : 'https://spotify-podcast-switcher-v3-production.up.railway.app';
+
+  // Configure the proxy middleware
+  app.use(
+    '/auth/**',
+    createProxyMiddleware({
+      target,
+      changeOrigin: true,
+    })
+  );
 };
